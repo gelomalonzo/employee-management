62663f0f5b4fab682602c0cfdbb150832a1b27dd
gelomalonzo <malonzoab@gmail.com>
2025-05-26 11:06:52 +0800

    Cleaned codes

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 	await loadAlerts('alertContainer');
 	
 	if (sessionStorage.getItem('loginError') === 'true') {
-		console.log('Login Error Test');
 		showErrorAlert('Invalid credentials.');
 		sessionStorage.removeItem('loginError');
 	}
